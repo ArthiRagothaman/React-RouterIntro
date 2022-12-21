cf34842c9de6723e519f9667453c6694f989ae58
ArthiRagothaman <arthi@example.com>
2022-12-21 21:05:00 +0100

    initial commit

---
+++ src/routes/About.tsx
@@ -0,0 +1,11 @@
+export function About() {
+	return (
+		<div>
+			<p>About</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Asperiores maiores explicabo libero.
+			</p>
+		</div>
+	);
+}
---
+++ src/routes/Home.tsx
@@ -0,0 +1,12 @@
+export function Home() {
+	return (
+		<div>
+			<h1>Home</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Quisquam sed nam ad dolorum quas porro natus quasi recusandae
+				quis alias.
+			</p>
+		</div>
+	);
+}
